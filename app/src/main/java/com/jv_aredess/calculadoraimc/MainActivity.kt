5c6f3c4b9b414f06a300f0a648e6b9f2d9d8808e
jv_aredess <joaovitoraredes.jvv@gmail.com>
2021-08-28 16:03:52 -0300

    Created UI interation

--- app/src/main/java/com/jv_aredess/calculadoraimc/MainActivity.kt
+++ app/src/main/java/com/jv_aredess/calculadoraimc/MainActivity.kt
@@ -1,19 +1,100 @@
 package com.jv_aredess.calculadoraimc
 
-import androidx.appcompat.app.AppCompatActivity
+import android.annotation.SuppressLint
 import android.os.Bundle
-import android.util.Log
-import android.view.View
-import android.view.View.*
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.widget.doAfterTextChanged
+import androidx.core.widget.doOnTextChanged
+import kotlinx.android.synthetic.main.activity_main.*
+import java.text.DecimalFormat
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        Log.w("lifecicle","CREATE - estou criando a tela")
+        setListeners()
     }
 
+    private fun setListeners() {
+        alturaEDT?.doAfterTextChanged { text ->
+        //    Toast.makeText(this, text, Toast.LENGTH_SHORT).show()
+        }
+        pesoEDT?.doOnTextChanged { text, _, _ ,_ ->
+        //   titleTXT?.text = text
+        }
+        calcularBTN?.setOnClickListener {
+            calcularIMC(pesoEDT.text.toString(), alturaEDT.text.toString())
+        }
+    }
+
+    @SuppressLint("SetTextI18n")
+    private fun calcularIMC(peso: String, altura:String){
+        val dec = DecimalFormat("#,###.00") //2 casas decimais no final separado milhar "." e decimais ","
+        val peso = peso.toFloatOrNull()
+        val altura = altura.toFloatOrNull()
+        if(peso != null && altura != null) {
+            val imc = peso / (altura * altura)
+
+            if (imc<17){
+                titleTXT.text = "${dec.format(imc)} \nmuito abaixo do peso"
+            }
+            else if (imc>=17 && imc<18.49){
+                titleTXT.text = "${dec.format(imc)} \nabaixo do peso"
+            }
+            else if (imc>=18.5 && imc<24.99){
+                titleTXT.text = "${dec.format(imc)} \npeso normal"
+            }
+            else if (imc>=25 && imc<29.99){
+                titleTXT.text = "${dec.format(imc)} \nacima do peso"
+            }
+            else if (imc>=30 && imc<34.99){
+                titleTXT.text = "${dec.format(imc)} \nobesidade I"
+            }
+            else if (imc>=35 && imc<39.99){
+                titleTXT.text = "${dec.format(imc)} \nobesidade II"
+            }
+            else{
+                titleTXT.text = "${dec.format(imc)} \nobesidade III"
+            }
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    /*
     override fun onStart(){
         super.onStart()
         Log.w("lifecicle","entrei no START - deixei a tela visível para você")
@@ -42,4 +123,4 @@ class MainActivity : AppCompatActivity() {
         super.onDestroy()
         Log.w("lifecicle","\nDESTROY - oh não! a tela foi destruída")
     }
-}
+*/
